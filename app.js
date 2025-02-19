2df1420570dd4e0170a4abf00e7d3f02ab7999e6
vladimir <vmunozc@hotmail.com>
2025-02-19 18:24:46 -0300

    modificar

--- app.js
+++ app.js
@@ -68,65 +68,32 @@ function sortearAmigo() {
         }
    return;
 }
+function validar(){
+let caja1 = nombreDeAmigo
 
-// La siguiente funcion valida el elemento input
-function validar(nombreDeAmigo) {
-    // Variable que usaremos para determinar si el input es valido
-    let isValid = false;
-
-    // El input que queremos validar
-    //const input = document.forms['validationForm']['letras'];
-    let input = nombreDeAmigo;
-    //El div con el mensaje de advertencia:
-    const message = document.getElementById('message');
-
-    input.willValidate = false;
-
-    // El tamaño maximo para nuestro input
-    const maximo = 15;
-
-    // El pattern que vamos a comprobar
-    const pattern = new RegExp('^[A-Z]+$', 'i');
-
-    // Primera validacion, si input esta vacio entonces no es valido
-    if(!input.value) {
-      isValid = false;
-    } else {
-      // Segunda validacion, si input es mayor que 15
-      if(input.value.length > maximo) {
-        isValid = false;
-      } else {
-        // Tercera validacion, si input contiene caracteres diferentes a los permitidos
-        if(!pattern.test(input.value)){ 
-        // Si queremos agregar letras acentuadas y/o letra ñ debemos usar
-        // codigos de Unicode (ejemplo: Ñ: \u00D1  ñ: \u00F1)
-          isValid = false;
-        } else {
-          // Si pasamos todas la validaciones anteriores, entonces el input es valido
-          isValid = true;
-        }
-      }
+if (caja1.length == 35 || /^\s+$/.test(caja1)) 
+    {
+        alert("Sólo se permiten de 35 letras para el nombre")
+        return false;
     }
 
-    //Ahora coloreamos el borde de nuestro input
-    if(!isValid) {
-      // rojo: no es valido
-      input.style.borderColor = 'salmon'; // me parece que 'salmon' es un poco menos agresivo que 'red'
-      // mostramos mensaje
-      message.hidden = false;
-    } else {
-      // verde: si es valido
-      input.style.borderColor = 'palegreen'; // 'palegreen' se ve mejor que 'green' en mi opinion
-      // ocultamos mensaje;
-      message.hidden = true;
+    else if (caja1.length <= 35 || /^\s+$/.test(caja1)) 
+    {
+        document.getElementById("txtnombre").focus();
+        document.getElementById("txtnombre").style.borderColor="green";
+        return true;    
     }
 
-    // devolvemos el valor de isValid
-    return isValid;
-  }
-
+    else if (/^([0-9])*$/.test(caja1))
+    {
+        alert("El valor " + caja1 + " no es una letra");
+        document.getElementById("txtnombre").focus();
+        document.getElementById("txtnombre").style.borderColor="red";
+        return false; 
+    }
+}
   // Función que verifica si el campo es válido antes de realizar cualquier otra acción.
-  function verificar() {
+  function verificar(nombreDeAmigo) {
     const valido = validar();
     if (!valido) {
       alert('El campo no es válido.');
